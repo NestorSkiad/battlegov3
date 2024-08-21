c26bb211b9fe7f725fd87f90446d630e7e766fbe
NestorSkiad <nestoras_skiadas@protonmail.com>
2024-08-21 20:26:37 +0200

    started board init, going for lunch

--- gamelogic.go
+++ gamelogic.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// boilerplate 5ever
 type Matrix[T any] struct {
 	w, h int
 	data []T
@@ -15,4 +23,47 @@ func (m Matrix[T]) At(x, y int) T{
 
 func (m Matrix[T]) Set(x, y int, t T) {
 	m.data[y*m.w+x] = t
+}
+
+type Direction bool
+
+const (
+	North Direction = true
+	South Direction = true
+	East Direction = true
+	West Direction = true
+)
+
+type Ship struct {
+	startx, starty, endx, endy int
+}
+
+func getEndCoords(startx, starty, boardx, boardy, length int, dir Direction, c *gin.Context) (int, int, error) {
+	outOfBoundsError := errors.New("ship is out of bounds")
+
+	switch dir {
+	case North:
+		if endy := starty + length; endy > boardy {
+			return 0, 0, outOfBoundsError
+		} else {
+			return startx, endy, nil
+		}
+	case South:
+		return startx, starty - length, nil
+	case East:
+		return startx + length, starty, nil
+	case West:
+		return startx - length, starty, nil
+	}
+
+	c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "no username supplied"})
+	return 0, 0, errors.New("")
+}
+
+type Board struct {
+
+}
+
+func newBoard(w, h int, s ...Ship) {
+
 }
