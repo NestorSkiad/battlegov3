64c5ab0a1e883c742709464676c074f2dd85467c
NestorSkiad <nestoras_skiadas@protonmail.com>
2024-08-21 19:12:59 +0200

    slice is already a pointer (facepalm)

--- gamelogic.go
+++ gamelogic.go
@@ -1,1 +1,18 @@
 package main
+
+type Matrix[T any] struct {
+	w, h int
+	data []T
+}
+
+func MakeMatrix[T any](w, h int) Matrix[T] {
+	return Matrix[T]{w, h, make([]T, w*h)}
+}
+
+func (m Matrix[T]) At(x, y int) T{
+	return m.data[y*m.w+x]
+}
+
+func (m Matrix[T]) Set(x, y int, t T) {
+	m.data[y*m.w+x] = t
+}
